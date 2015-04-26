158464e7e6bcc8c8bbcc2c81d538e549f380b5d2
Petar Mladenovic <petar@madjionicarskitrikovi.com>
2015-04-26 11:49:24 +0200

    confirm v2

--- controllers/Confirm.php
+++ controllers/Confirm.php
@@ -35,19 +35,12 @@ class ConfirmController extends Controller {
             ->title('Confirmation')
             ->content
             (
-                '<div id="home" class="static-header small-header light">
-                    <div class="text-heading"></div>
-                </div>
-                <section class="confirm-section">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <p class="response">'.$msg.'</p>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <a id="showHere"></a>'
+                '<div id="home" class="static-confirm static-header light">
+                    <div class="text-heading">
+                    <p class="confirm-response">'.$msg.'</p></div>
+                    <a id="showHere"></a>
+                </div>'
+
             )
         ->render();
     }
